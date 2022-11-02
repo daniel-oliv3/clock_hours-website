b9a776f4f83a25f01ccbda88860710aad2b5c1f4
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-02 10:30:43 -0300

    Update ok

--- js/script.js
+++ js/script.js
@@ -3,15 +3,21 @@ const min = document.querySelector(".min");
 const sec = document.querySelector(".sec");
 
 
-function getTime {
+function getTime() {
     const time = new Date();
 
     const getHourRot = (360 / 12) * time.getHours();
     const getMinRot = (360 / 60) * time.getHours();
     const getSecRot = (360 / 60) * time.getSeconds();
+
+    hour.style.transform = `rotate(${getHourRot}deg)`;
+    min.style.transform = `rotate(${getMinRot}deg)`;
+    sec.style.transform = `rotate(${getSecRot}deg)`;
 }
 
-
+setInterval(() => {
+    getTime();
+}, 1000);
 
 
 
